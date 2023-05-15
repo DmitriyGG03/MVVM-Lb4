891b7535807dc244c3159bbfe3c02a4c4eac52e3
DmitriyGG03 <dimchikdg89@gmail.com>
2023-05-15 17:43:32 +0300

    Implement Ui models

--- MVVM-Lb4.WPF/UIModels/UIGroup.cs
+++ MVVM-Lb4.WPF/UIModels/UIGroup.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
+using MVVM_Lb4.Domain.Models;
+
 namespace MVVM_Lb4.UIModels;
 
 public class UIGroup
 {
-    
+    public Guid GroupId { get; set; }
+    public string GroupName { get; set; }
 }
--- MVVM-Lb4.WPF/UIModels/UIStudent.cs
+++ MVVM-Lb4.WPF/UIModels/UIStudent.cs
@@ -1,6 +1,15 @@
+using System;
+
 namespace MVVM_Lb4.UIModels;
 
 public class UIStudent
 {
-    
+    public Guid StudentId { get; set; }
+    public string Name { get; set; }
+
+    public string LastName { get; set; }
+
+    public string Patronymic { get; set; }
+
+    public byte CourseNumber { get; set; }
 }
